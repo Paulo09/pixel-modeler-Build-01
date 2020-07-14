b321eab4d3aeca70224e9611b041260257f6cfb5
Paulo09 <paullocasttro@hotmail.com>
2020-07-13 23:16:24 -0300

    Gerador Files Issue

--- grails-app/controllers/pixel/modeler/GerararquivoController.groovy
+++ grails-app/controllers/pixel/modeler/GerararquivoController.groovy
@@ -136,121 +136,52 @@ class GerararquivoController {
 	
 	def selecionartabelas (Gerararquivo gerararquivo) {
 	
-		def cliente="";def tabelas=[];def cont=0;def selectCampos=""; def dataBase="";
+		def list = [];def cliente="";def tabelas=[];def cont=0;def selectCampos=""; def dataBase="";def bases="";def urlLista="";
 		
-		//if()
-		
-		//println "--------------------------------- Morre -----------------------"+params.nometabela
-		
-		if(params.nometabela != null && params.nometabela.size() > 1){
-		
-		
-			def list = []
-			list.add(params.nometabela.toString().replace("[","['").replace("]","']").replace(" ","").replace(",","','"))	
-            if(list.size() == 1){			
-				println "---------- Singular ------"+list.toString().replace("[","['").replace("]","']").replace("'']","").replace("[''","")
-			}else{
-			    println "------------------------- Plural ------------------"+list.size() //params.nometabela.toString().replace("[","['").replace("]","']").replace(" ","").replace(",","','")
-			}	
-
-		
-		     //def sss = params.nometabela.size()
-			 
-			 //println "--------------- Arbusto -----------"+sss
-		   
-			 //cliente = params.nometabela
-			 //println "------------------------- Salvo ------------------"+params.nometabela.toString().replace("[","['").replace("]","']").replace(" ","").replace(",","','")
+		if(params.nometabela != null && params.nometabela.size() > 1){		
 			
+			list.add(params.nometabela.toString().replace("[","['").replace("]","']").replace(" ","").replace(",","','"))
+			
+			//
+			//Criar file:
+			File file = new File("C:\\walter\\paulo.groovy");					
+			def criarapp = new FileWriter(new File("C:\\walter\\paulo.groovy"));	
+			criarapp.write("grails.plugin.reveng.includeTables = "+list.toString().replace("[","['").replace("]","']").replace("'']","").replace("[''","")+"\n");
+			criarapp.write("grails.plugin.reveng.destDir = 'diretorioAplicacao/chico/grails-app/domain'");	
+			criarapp.close();
+			
+			//
+			//Criar Controller - dinamicamente
+			def contControler=0
+			params.nometabela.each{
+				urlLista += '   "/'+params.nometabela[contControler]+'"'+"(resources:"+" '"+params.nometabela[contControler]+"')"+"\n"
+				
+				File file2 = new File("C:\\walter\\controller${params.nometabela[contControler]}.groovy");					
+				def criarConrtoller = new FileWriter(new File("C:\\walter\\controller${params.nometabela[contControler]}.groovy"));	
+				criarConrtoller.write("package rest.api.docs\nimport com.wordnik.swagger.annotations.Api\nimport grails.rest.RestfulController\n@Api(value = '${params.nometabela[contControler]}', description = '${params.nometabela[contControler]} Management  API')\nclass ${params.nometabela[contControler]}Controller extends RestfulController {\nstatic responseFormats = ['json', 'xml']\nUserController(){\nsuper(User)\n}}");	
+				criarConrtoller.close();
+				contControler++
+			}
+			
+			    File file3 = new File("C:\\walter\\UrlMappings.groovy");					
+				def criarUrlMaps = new FileWriter(new File("C:\\walter\\UrlMappings.groovy"));	
+				criarUrlMaps.write('package rest.api.docs\nclass UrlMappings {\n static mappings = {\n   "/$controller/$action?/$id?(.$format)?"{constraints{// apply constraints here}}\n   "/"(controller: "application", action:"index")\n   "500"(view: "/application/serverError")\n   "404"(view: "/application/notFound")\n'+urlLista+' }\n}');
+				criarUrlMaps.close();
 		
+
         }
 		
 		try {
-		
 			def conexao = Sql.newInstance("jdbc:postgresql://localhost:5432/$gerararquivo.base.nome","postgres", "root", "org.postgresql.Driver")
 			def selectBaseOrigem = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+			def selectBases = "SELECT datname FROM pg_database WHERE datistemplate = 'f'"
+			bases = conexao.rows(selectBases).datname
 			tabelas = conexao.rows(selectBaseOrigem).table_name
 			def gerarClasse=""
-			dataBase = gerararquivo.base.nome	
+			dataBase = gerararquivo.base.nome			
+		}catch(Exception e){}		
 
-
-			println "-------------------------------- Valor Size *** ----------------------------"+params.gerararquivo
-			
-			
-			if(params.nometabela.size() > 1){
-				
-				params.nometabela.each{
-				    def dd=0;def campos=""; 
-					
-					selectCampos = "SELECT data_type,column_name FROM information_schema.columns WHERE table_name in ('${params.nometabela[cont]}')".toString()
-					gerarClasse=conexao.rows(selectCampos)
-					
-					    //Seleciona - maior que uma opção
-						//Verifica as tabelas selecionadas e faz a transpilação da mesma.Mais de uma opção selecionada.
-						//Obs: Para este caso, mais de uma tabela selecionada.
-						//Autor: @PauloCastro 
-						if(gerarClasse){
-					
-						   gerarClasse.each{
-						  
-						        gerarClasse
-								
-								def objClasse = "class "+"${params.nometabela[cont]}"+" {\n"+
-									gerarClasse[dd].toString().replace('column_name=','').replace('{data_type=','').replace('character','String').replace('character varying','String').replace('varying,','').replace('integer','Integer').replace(', ',' ').replace('}','').replace('bigint','Integer') //.replace('version','').replace('{data_type=','').replace('integer','Integer').replace('character','String').replace('character varying','String').replace('{data_type=character','String').replace(' column_name=','').replace('}','').replace(',',' ')+
-								"\n}";
-								
-								File file = new File("grails-app\\classes\\${params.nometabela[cont]}.groovy");					
-								if(!file.exists())
-								{						
-								def criarapp = new FileWriter(new File("grails-app\\aplicacao\\${params.nometabela[cont]}.groovy"));	
-								    criarapp.write(objClasse); 
-								    criarapp.close();
-								}
-								dd++
-							}
-							
-						}
-						//Seleciona - uma única opção 
-						//Verifica a tabela selecionada e faz a transpilação da mesma.Somente uma opção, somente uma tabela.
-						//Obs: Para este caso, única tabela.
-						//Autor: @PauloCastro
-						if(!gerarClasse){
-						    
-							    File diretorio = new File("grails-app\\domain\\${gerararquivo.base.nome.replace('_','')}");
-								diretorio.mkdir();
-								
-								println "-------------------------------- Valor 3 ----------------------------"+$gerararquivo.base.nome
-			
-							    selectCampos = "SELECT data_type,column_name FROM information_schema.columns WHERE table_name in ('${params.nometabela}')".toString()
-								gerarClasse=conexao.rows(selectCampos)
-								
-								
-								
-								def con1=0;def res=""
-								gerarClasse.each{
-								   res += ' '+gerarClasse[con1].toString().replace('column_name=','').replace('{data_type=','').replace('character','String').replace('character varying','String').replace('varying,','').replace('integer','Integer').replace(', ',' ').replace('}','').replace('bigint','Integer').replace('_','').replace('timestamp without time zone','Date').replace('text','String')+"\n"
-								   con1++
-								}
-								
-								def objClasse = "package ${gerararquivo.base.nome.replace('_','')} \n"+"import grails.rest.* \n"+"@Resource(uri='/api/${params.nometabela.toLowerCase().replace('_','')}') \n"+"""class ${params.nometabela.substring(0,1).toUpperCase()}${params.nometabela.substring(1,params.nometabela.size()).toLowerCase().replace('_','')}{\n${res}}
-												"""
-								
-								File file = new File("${diretorio}\\${params.nometabela.substring(0,1).toUpperCase()}${params.nometabela.substring(1,params.nometabela.size()).toLowerCase().replace('_','')}.groovy");					
-								if(!file.exists())
-								{						
-								def criarapp = new FileWriter(new File("${diretorio}\\${params.nometabela.substring(0,1).toUpperCase()}${params.nometabela.substring(1,params.nometabela.size()).toLowerCase().replace('_','')}.groovy"));	
-								    criarapp.write(objClasse); 
-								    criarapp.close();
-								}
-						  
-						 }
-						
-					}   
-					
-				}
-		  
-		}catch(Exception e){}	
-
-		render view:'selecionartabelas.gsp', model:[tabelas:tabelas,dataBase:dataBase]
+		render view:'selecionartabelas.gsp', model:[tabelas:tabelas,dataBase:dataBase,bases:bases]
 	
 	}
 	
